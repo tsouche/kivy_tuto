45baa0722ae9336ef4e10d26d72b246c7d9cf205
Thierry Souche <thierry.souche@gmail.com>
2016-10-06 15:38:55 +0200

    Start making the cards 'modifiable'

--- chapitre1/06 - embebbed layout/set.py
+++ chapitre1/06 - embebbed layout/set.py
@@ -7,14 +7,27 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.button import Button
-from kivy.properties import BoundedNumericProperty
+from kivy.properties import NumericProperty, BoundedNumericProperty
 from kivy.properties import StringProperty
 from random import randint
 
-
+constant_card_height = 150
+constant_card_width  = 100
+constant_nb_cols = 4
+constant_nb_rows = 3
+constant_spacing = 10
+constant_padding = 10
+constant_table_width  =  constant_card_width *  constant_nb_cols      \
+                      +     constant_spacing * (constant_nb_cols - 1) \
+                      +     constant_padding * 2
+constant_table_height = constant_card_height *  constant_nb_rows      \
+                      +     constant_spacing * (constant_nb_rows - 1) \
+                      +     constant_padding * 2
 
 class Card(Button):
 
+    card_width = NumericProperty()
+    card_height = NumericProperty()
     i = BoundedNumericProperty(0, min=0, max=4)
     j = BoundedNumericProperty(0, min=0, max=3)
     c = BoundedNumericProperty(0, min=0, max=3)
@@ -30,6 +43,8 @@ class Card(Button):
         # filepath = "/data/code/setgame/client/images/"
         filepath = "./../../../setgame/client/images/"
         super(Card, self).__init__()
+        self.card_width  = constant_card_width
+        self.card_height = constant_card_height
         self.i = ic
         self.j = jc
         self.code = card_code
@@ -38,7 +53,7 @@ class Card(Button):
         self.f = int(card_code[2])
         self.n = int(card_code[3])
         self.filename = filepath + self.code + ".png"
-        #print("BOGUS00: ", self.filename)
+        print("BOGUS00: i=", ic, "- j=", jc, ":", self.filename)
         
     def build(self):
         return self
@@ -53,15 +68,38 @@ class PositionOnTable(RelativeLayout):
         return self
 
 class Table(GridLayout):
-    pass
+    
+    def index(self, i, j):
+        return i + j*constant_nb_cols
+    
+    def __init__(self):
+        super(Table, self).__init__()
+        self.size = (constant_table_width, constant_table_height)
+        self.cols = constant_nb_cols
+        self.rows = constant_nb_rows
+        self.col_default_width  = constant_card_width
+        self.row_default_height = constant_card_height
+        self.positions = []
+        for j in range(0,constant_nb_rows):
+            for i in range(0,constant_nb_cols):
+                code = str(randint(0,2))+str(randint(0,2))+str(randint(0,2))+str(randint(0,2))
+                self.positions.append(PositionOnTable(i,j,code))
+        for j in range(0,constant_nb_rows):
+            for i in range(0,constant_nb_cols):
+                self.add_widget(self.positions[self.index(i,j)])
+
+    def setCardCode(self, i,j, code):
+        """
+        This method resets the card code of a card on the table.
+        If code == -1: the position (i,j) is empty.
+        """
+        """ pure test code for teh moment"""
+        pass
+        
 
 class SetApp(App):
     def build(self):
         table = Table()
-        for j in range(0,3):
-            for i in range(0,4):
-                code = str(randint(0,2))+str(randint(0,2))+str(randint(0,2))+str(randint(0,2))
-                table.add_widget(PositionOnTable(i,j,code))
         return table
 
 if __name__=="__main__":
